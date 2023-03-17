5e52016432417f32e633993c4b439848e49a3e3e
quro4ka <quro4ka@gmail.com>
2023-03-17 18:05:12 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import bridge from '@vkontakte/vk-bridge'
+import App from './App'
+
+// Init VK  Mini App
+bridge
+  .send('VKWebAppInit')
+  .then((data) => {
+    if (data.result) {
+      console.log('iniiit')
+    } else {
+      console.log('err')
+    }
+  })
+  .catch((error) => {
+    // Ошибка
+    console.log(error)
+  })
+
+ReactDOM.render(<App />, document.getElementById('root'))
+if (process.env.NODE_ENV === 'development') {
+  import('./eruda').then(({ default: eruda }) => {}) //runtime download
+}
